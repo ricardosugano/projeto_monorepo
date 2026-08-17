57781d1d72c66f74454b632148f5589b57acf3a1
ricardosugano <fatec-dsm4@academico.intra>
2026-08-17 20:58:22 -0300

    teste

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -3,14 +3,30 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const isSSL = process.env.DB_SSL === "true";
+
+const sequelizeOptions = {
+  host: process.env.DB_HOST_LOCAL || "localhost",
+  port: parseInt(process.env.DB_PORT_LOCAL || "5432", 10),
+  dialect: "postgres",
+  logging: false,
+  dialectOptions: isSSL
+    ? {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      }
+    : {},
+
+};
+
 export const sequelize = new Sequelize(
   process.env.DB_NAME || "postgres",
   process.env.DB_USER || "postgres",
   process.env.DB_PASSWORD || "",
-  {
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT || 5432),
-    dialect: "postgres",
-    logging: false,
-  },
+  sequelizeOptions  
+
+  
+
 );
---
+++ backend/src/models/Users.ts
@@ -0,0 +1,39 @@
+import { Model, DataTypes } from "sequelize";
+import { sequelize } from "../config/database";
+
+export class User extends Model {
+  public id!: number;
+  public name!: string;
+  public email!: string;
+  public senha_hash!: string;
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    name: {
+      type: DataTypes.STRING(100),
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      unique: true
+    },
+    senha_hash: {
+      type: DataTypes.STRING(255),
+      allowNull: false
+    }
+  },
+  {
+    sequelize,
+    tableName: "users",
+    timestamps: true
+  }
+);
