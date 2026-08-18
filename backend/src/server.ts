332a83b7615799fd103aa057cba73bb82d99d4bd
ricardosugano <sugano.ricardo@gmail.com>
2026-08-17 21:59:40 -0300

    terc

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,7 @@ import express, { Express, Request, Response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { sequelize } from './config/database';
+import { User } from "./models/Users";
 
 dotenv.config();
 
@@ -20,6 +21,23 @@ app.get("/api/health",  (req: Request, res: Response) => {
         timestamp: new Date().toISOString()
     });
 });
+
+// cadastrar um novo usuário
+app.post("/api/users", async (req: Request, res: Response) => {
+    try {
+        const { name, email, password } = req.body;
+        if (!name || !email || !password) {
+            return res.status(400).json({ message: 'Nome, email e senha são obrigatórios' });
+        }
+
+        const novoUsuario = await User.create({ name, email, password });
+        res.status(201).json(novoUsuario);
+        
+    } catch (error) {
+        res.status(500).json({ message: 'Erro ao criar usuário' });
+    }
+});
+
 
 async function main() {
     try {
